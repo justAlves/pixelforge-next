4816bc9d0692d3b536021c2887304a504ebc4345
Guilherme Alves <iamalves.jpg@gmail.com>
2025-02-04 20:04:56 -0300

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="h-screen bg-black/85 flex items-center justify-center">
-      <div className="bg-white/20 backdrop-blur-md p-8 rounded-lg shadow-lg">
-        <h1 className="text-4xl text-white font-bold font-orbitron">Hello World Transparente!</h1>
-      </div>
+    <div className="h-screen flex flex-col items-start px-8 bg-background lg:px-32">
+      <section className="pt-64">
+        <h1 className="text-5xl font-bold font-orbitron">
+          Onde <span className="text-app_primary">Criadores</span> e <span className="text-app_primary">Jogadores</span><br/> se Encontram!
+        </h1>
+        <p className="text-lg font-inter mt-4">
+          Uma comunidade feita para desenvolvedores indie venderem,<br/> compartilharem e crescerem juntos.
+        </p>
+        <Button
+          className="mt-8"
+        >
+          Crie sua Conta Grátis
+        </Button>
+        <Button
+          className="ml-4"
+          variant="outline"
+          size={"sm"}
+        >
+          Explorar Marketplace
+        </Button>
+      </section>
     </div>
   );
 }
